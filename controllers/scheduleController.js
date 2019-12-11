d69bc80ba6d85a2f444671cfc00d5b884dcaf59b
Elijah Magallanes <Elijah.Magallanes@hunterindustries.com>
2019-12-11 07:40:19 -0800

    Extracted function for readability

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,11 +1,16 @@
 function scheduleController(schedule) {
 
+    function createNewMonthOption(document, month) {
+        const monthOption = document.createElement('option');
+        monthOption.value = month;
+        const monthTextNode = document.createTextNode(month);
+        monthOption.appendChild(monthTextNode);
+        return monthOption;
+    }
+
     function addSelectorOptions(monthSelector, document) {
         Object.keys(schedule).forEach(month => {
-            const monthOption = document.createElement('option');
-            monthOption.value = month;
-            const monthTextNode = document.createTextNode(month);
-            monthOption.appendChild(monthTextNode);
+            const monthOption = createNewMonthOption(document, month)
             monthSelector.appendChild(monthOption);
         });
     }
