622418ac4906b9258316c38019dcdd3196e0d7cd
HUNTER\EMagallanes <elijah.magallanes@hunterindustries.com>
2020-01-06 07:11:36 -0800

    Updating for 2020

--- filebase/scheduleFilebase.js
+++ filebase/scheduleFilebase.js
@@ -1,3 +1,3 @@
-const schedule = {"JAN":[],"FEB":[],"MARCH":[],"APRIL":[],"MAY":[],"JUNE":[],"JULY":[],"AUG":[],"SEPT":[],"OCT":[],"NOV":[],"DEC":[{"22":"VAR","TEAM":"SAINTS"}]};
+const schedule = {"JAN":[{"5":"VAR","9":"CA","12":"VAR","19":"VAR","26":"VAR","TEAM":"SAINTS"}],"FEB":[],"MARCH":[],"APRIL":[],"MAY":[],"JUNE":[],"JULY":[],"AUG":[],"SEPT":[],"OCT":[],"NOV":[],"DEC":[]};
 
 module.exports = schedule;
